0a63cef2347d05f97b8b6bfc8ab35f1c52317aaa
Ethem Kizilyer <ethemkizilyer3546@gmail.com>
2025-07-17 00:06:01 +0300

    er

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
-import Page from "./components/test";
+import { Button } from "rizzui/button";
+
 
 export default function App() {
-
-  return <div>
-    <Page/>
-    </div>;
+  return (
+    <div>
+      <Button variant="solid" color="primary">
+        Primary Button
+      </Button>
+      <Button variant="outline" color="secondary">
+        Secondary Button
+      </Button>
+    </div>
+  );
 }
