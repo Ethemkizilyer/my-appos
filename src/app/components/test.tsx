59ea5927749795daefe58c37649a84a2de5fba03
Ethem Kizilyer <ethemkizilyer3546@gmail.com>
2025-07-17 00:03:40 +0300

    asd

---
+++ src/app/components/test.tsx
@@ -0,0 +1,15 @@
+import { Button } from "rizzui/button";
+
+
+export default function MyComponent() {
+  return (
+    <div>
+      <Button variant="solid" color="primary">
+        Primary Button
+      </Button>
+      <Button variant="outline" color="secondary">
+        Secondary Button
+      </Button>
+    </div>
+  );
+}
